2fec57dfac4220cce57bf603b366b17630829481
Jaswant195 <Jashobanta195@gmail.com>
2017-07-06 14:01:51 +0530

    First commit

---
+++ DemoApplication4/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DemoApplication4.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DemoApplication4/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoApplication4.Startup))]
+namespace DemoApplication4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
